2be5c01ebae4642d3c9ca45a06f21c5217fbdc5f
Anna <ankaburkat@gmail.com>
2017-08-06 10:53:08 +0200

    border-radius; autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
-var sourcemaps= require('gulp-sourcemaps');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 
-gulp.task('reload', function(){
+gulp.task('reload', function () {
   browserSync.reload();
 });
 
 
-gulp.task('serve', ['sass'], function(){
+gulp.task('serve', ['sass'], function () {
   browserSync({
     server: 'src'
   });
@@ -18,25 +19,17 @@ gulp.task('serve', ['sass'], function(){
   gulp.watch('src/scss/**/*.scss', ['sass'])
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function () {
   return gulp.src('src/scss/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('src/css'))
-  .pipe(browserSync.stream());
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
 })
-
-
-gulp.task('css', function () {
-    var postcss    = require('gulp-postcss');
-
-    return gulp.src('src/**/*.css')
-        .pipe( sourcemaps.init() )
-        .pipe( postcss([ require('precss'), require('autoprefixer') ]) )
-        .pipe( sourcemaps.write('.') )
-        .pipe( gulp.dest('build/') );
-});
 
 
 gulp.task('default', ['serve']);
